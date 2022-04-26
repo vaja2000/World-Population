eb7f80b1f07ac52399973b03c05ce091e0335170
vaja <74463035+vaja2000@users.noreply.github.com>
2022-04-26 04:08:50 +0400

    population

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocationComponent } from './views/location/location.component';
+import { PopulationComponent } from './views/population/population.component';
+import { OtherDataComponent } from './views/other-data/other-data.component';
+import { MenuComponent } from './views/menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationComponent,
+    PopulationComponent,
+    OtherDataComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/other-data/other-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-other-data',
+  templateUrl: './other-data.component.html',
+  styleUrls: ['./other-data.component.scss']
+})
+export class OtherDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
